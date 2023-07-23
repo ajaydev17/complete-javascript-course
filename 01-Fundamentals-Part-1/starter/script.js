3ac3d8d6beac030b6660eccb77ff3560e65824ec
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-24 02:00:34 +0530

    javascript data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,26 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is Fun!!");
 console.log(17 + 29 - 50 + 40);
+
+// most important data type: integer, string, and boolean
+let isJavascript = true;
+console.log(isJavascript);
+console.log(typeof isJavascript);
+
+isJavascript = "YES!!";
+console.log(isJavascript);
+console.log(typeof isJavascript);
+
+isJavascript = 1;
+console.log(isJavascript);
+console.log(typeof isJavascript);
+
+// undefined data type
+let firstName;
+console.log(firstName);
+console.log(typeof firstName);
+
+// null data type
+let lastName = null;
+console.log(lastName);
+console.log(typeof lastName);
