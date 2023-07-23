865fb878c1bbe59ad3e7aa45587b1d86b21e34d3
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-24 02:27:21 +0530

    let, const and var type declaration

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -24,3 +24,33 @@ console.log(typeof firstName);
 let lastName = null;
 console.log(lastName);
 console.log(typeof lastName);
+
+// let, const and var variable declaration
+
+// use let when we sure that the variable value will change in the execution
+let age = 30;
+age = 31;
+console.log(age);
+
+// use const when we know that the variable value wont change in the execution
+// const is the best variable declaration as using variable with let may cause buggy code
+const birthYear = 1996;
+console.log(birthYear);
+
+// we cant change value of birthYear
+// birthYear = 1997; this will throw an error
+
+// var is the old way of declaring variables after ES6 it is not the preferred way of declaring variables but still works
+var job = "programming";
+job = "teaching";
+console.log(job);
+
+// var and let looks almost same but they are completly different below the surface.
+// let is block scoped
+// var has function scope
+
+// In javascript we can initilize variable without type
+lastName = "Devadiga";
+console.log(lastName);
+
+// above code works fine but it creates lastName is global scope not in the current scope, so this is not the preferred way.
