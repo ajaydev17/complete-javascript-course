40e8baf73a403c63c0e643f000a380d82380e55c
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-19 23:54:28 +0530

    conditional statement example

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -5,18 +5,3 @@ let population = "140.76 crores";
 console.log(country);
 console.log(continent);
 console.log(population);
-
-// BMI calculator assignement
-
-/* Write your code below. Good luck! 🙂 */
-const massMark = 78;
-const heightMark = 1.69;
-
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
-
-console.log(BMIMark, BMIJohn, markHigherBMI);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,3 +54,24 @@ lastName = "Devadiga";
 console.log(lastName);
 
 // above code works fine but it creates lastName is global scope not in the current scope, so this is not the preferred way.
+
+// BMI calculator assignement
+
+/* Write your code below. Good luck! 🙂 */
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+}
