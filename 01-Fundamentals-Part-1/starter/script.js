0c4a8d6663e652a131ed9a3ab58f03bc249201f7
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-20 01:50:27 +0530

    type coercion example

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,3 +108,12 @@ console.log(typeof inputYear);
 console.log(Number("Jonas"));
 console.log(typeof NaN);
 console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
