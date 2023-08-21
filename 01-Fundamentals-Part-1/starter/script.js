c20db1d69e1468927df04739f20bb1d136d8f5ef
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-21 23:45:25 +0530

    truth and falsy value example

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -134,3 +134,10 @@ if (height) {
 } else {
     console.log("height is undefined");
 }
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
