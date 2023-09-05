69db94838eceff4ea3cf754fa8aa1ca719fec23b
Ajay Dev <devadigaajay1729@gmail.com>
2023-09-05 23:55:38 +0530

    javascript strict mode example

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+// use strict mode
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    // using this will throw error in strict mode
+    hasDriverLicense = true;
+}
+
+if (hasDriversLicense) {
+    console.log("You are eligible to ride!!");
+}
