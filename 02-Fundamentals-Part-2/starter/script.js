c87880072794003e04d9c273ad7b85de02823bff
Ajay Dev <devadigaajay1729@gmail.com>
2023-09-07 00:28:14 +0530

    javascript function example

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,7 +6,7 @@ const passTest = true;
 
 if (passTest) {
     // using this will throw error in strict mode
-    hasDriverLicense = true;
+    hasDriversLicense = true;
 }
 
 if (hasDriversLicense) {
@@ -14,5 +14,35 @@ if (hasDriversLicense) {
 }
 
 // this will throw the error as interface is a reserved keyword
-const interface = "strict mode example";
-console.log(interface);
+// const interface = "strict mode example";
+// console.log(interface);
+
+// const private = "we can not use private as variable name";
+// console.log(private);
+
+// const if = "we cant use if as variable name";
+// console.log(if);
+
+function logger() {
+    console.log("My name is Ajay");
+}
+
+logger();
+logger();
+logger();
+
+// function with parameters
+function fruitProcess(apple, orange) {
+    console.log(apple, orange);
+}
+
+fruitProcess(5, 2);
+
+// function with parameters and return value
+function fruitProcess2(apple, orange) {
+    const juice = `Juice with ${apple} apples and ${orange} oranges`;
+    return juice;
+}
+
+const result = fruitProcess2(5, 2);
+console.log(result);
